4c19a76584b70f22ef1485a575048e18732ab693
timperng <timperng@uw.edu>
2017-11-14 14:03:52 -0800

    finish scatterplot function

--- exercise-1/scripts/BuildScatter.R
+++ exercise-1/scripts/BuildScatter.R
@@ -1,4 +1,5 @@
 # Function to build a scatterplot
+library(ggplot2)
 
 # Write a function that returns a scatterplot, parameterizing the following variables:
 #   - Data to use
@@ -8,3 +9,10 @@
 #   - Title of the plot (set a default of "Title")
 #   - Label for the x axis (set a default of "X Title")
 #   - Label for the y axis (set a default of "Y Title")
+
+MakeScatter <- function(my.data, my.var, my.vector){
+  ggplot(data = my.data) + 
+    geom_point(mapping = aes(x = my.var, y = my.vector))
+}
+
+
